9518dbef3cc3890844b73281eb051a3fdef9346b
Tosaaaki <www.tosakiweb.net@gmail.com>
2025-06-08 00:15:28 +0900

    Add portfolio risk management engine

--- risk/__init__.py
+++ risk/__init__.py
@@ -1,4 +1,5 @@
 from .trade_guard import TradeGuard
 from .cvar import calc_cvar
+from .portfolio_risk_manager import PortfolioRiskManager
 
-__all__ = ["TradeGuard", "calc_cvar"]
+__all__ = ["TradeGuard", "calc_cvar", "PortfolioRiskManager"]
