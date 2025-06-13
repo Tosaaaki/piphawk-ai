b881a0d15728bb884665c9fcb23b07cc30c7102a
Tosaaaki <www.tosakiweb.net@gmail.com>
2025-06-13 13:03:31 +0900

    Add mode detector tests and adjust configs

--- analysis/__init__.py
+++ analysis/__init__.py
@@ -1,5 +1,6 @@
 from .llm_mode_selector import select_mode_llm
 from .mode_preclassifier import classify_regime
+from .mode_detector import detect_mode
 from .trade_patterns import calculate_trade_score
 
-__all__ = ["calculate_trade_score", "classify_regime", "select_mode_llm"]
+__all__ = ["calculate_trade_score", "classify_regime", "select_mode_llm", "detect_mode"]
